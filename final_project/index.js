ed04fc0c03bfe557bac3d2eaf3dbb01bc6a8f8c9
Apple <kashifumar349@gmail.com>
2023-05-11 21:18:45 +0500

    general.js completed

--- final_project/index.js
+++ final_project/index.js
@@ -1,22 +1,41 @@
-const express = require('express');
-const jwt = require('jsonwebtoken');
-const session = require('express-session')
-const customer_routes = require('./router/auth_users.js').authenticated;
-const genl_routes = require('./router/general.js').general;
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const session = require("express-session");
+const customer_routes = require("./router/auth_users.js").authenticated;
+const genl_routes = require("./router/general.js").general;
+const { users } = require("./router/auth_users");
 
 const app = express();
 
 app.use(express.json());
 
-app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
+app.use(
+  "/customer",
+  session({
+    secret: "fingerprint_customer",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
 
-app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+app.use("/customer/auth/*", function auth(req, res, next) {
+  //Write the authenication mechanism here
+  if (req.session.authorization) {
+    let token = req.session.authorization["token"];
+    // verify the token
+    jwt.verify(token, "access", (err, user) => {
+      if (err) {
+        return res.status(401).json({ mssage: "User not authenticated" });
+      } else {
+        next();
+      }
+    });
+  }
 });
- 
-const PORT =5000;
+
+const PORT = 3000;
 
 app.use("/customer", customer_routes);
 app.use("/", genl_routes);
 
-app.listen(PORT,()=>console.log("Server is running"));
+app.listen(PORT, () => console.log("Server is running"));
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,43 +1,68 @@
-const express = require('express');
+const express = require("express");
 let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
-
-public_users.post("/register", (req,res) => {
+public_users.post("/register", (req, res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.status(300).json({ message: "Yet to be implemented" });
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get("/", function (req, res) {
+  return res.status(200).send(JSON.stringify(books, null, 4));
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
+public_users.get("/isbn/:isbn", function (req, res) {
+  const isbn = req.params.isbn;
+  const book = books[isbn];
+  if (book) {
+    return res.status(300).json(book);
+  }
+  return res.status(400).json({ message: "Please provide valid ISBN." });
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
-  
+});
+
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get("/author/:author", function (req, res) {
+  const author = req.params.author;
+  // get keys of the books
+  const booksKeys = Object.keys(books);
+
+  for (const key of booksKeys) {
+    const bookData = books[key];
+    if (bookData.author == author) {
+      return res.status(200).json(bookData);
+    }
+  }
+  return res.status(400).json({ message: "Data not found" });
 });
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get("/title/:title", function (req, res) {
+  const title = req.params.title;
+  // get keys of the books
+  const booksKeys = Object.keys(books);
+
+  for (const key of booksKeys) {
+    const bookData = books[key];
+    if (bookData.title == title) {
+      return res.status(200).json(bookData);
+    }
+  }
+  return res.status(400).json({ message: "Data not found" });
 });
 
 //  Get book review
-public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get("/review/:isbn", function (req, res) {
+  const isbn = req.params.isbn;
+  const book = books[isbn];
+  if (book) {
+    return res.status(200).json(book.reviews);
+  }
+  return res.status(400).json({ message: "Data not found" });
 });
 
 module.exports.general = public_users;
